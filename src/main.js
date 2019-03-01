519ac0588fbf94444e8f42683f2c728e4d7be1b9
Minos Katevas <minos.kat@gmail.com>
2019-03-01 12:07:33 +0000

    Cleanup code

--- src/main.js
+++ src/main.js
@@ -55,7 +55,7 @@ app.get('/ui', function (req, res) {
 app.get('/ui/truelayer-redirect', (req, res) => {
   getSettings()
     .then(async (settings) => {
-      const { client_id, client_secret, redirect_url } = settings;
+      const { redirect_url } = settings;
       const { code } = req.query.code;
       const tokens = await client.exchangeCodeForToken(redirect_url, code);
       settings.tokens = tokens;
@@ -80,13 +80,13 @@ app.get('/ui/configure', async (req, res) => {
       // list them to the user
       res.type('html');
       // TODO: Use a pug template instead
-      res.write('<!doctype html>');
-      res.write('<html lang="en">');
-      res.write('<body>');
-      res.write('<h1>TrueLayer Driver Configuration</h1>');
-      res.write('<p>Please choose the account you want to monitor and its refresh interval:</p>');
-      res.write('<form action="/ui/saveConfiguration">');
-      res.write('Accounts:<br>');
+      res.write(`<!doctype html>
+                 <html lang="en">
+                 <body>
+                 <h1>TrueLayer Driver Configuration</h1>
+                 <p>Please choose the account you want to monitor and its refresh interval:</p>
+                 <form action="/ui/saveConfiguration">
+                 Accounts:<br>`);
 
       for(const account of accounts.results) {
         const { account_id, account_type, display_name } = account;
@@ -94,11 +94,11 @@ app.get('/ui/configure', async (req, res) => {
             <input type="radio" name="account" value="${account_id}"> ${display_name} (<i>${account_type}</i>)<br><br>
             `);
       }
-      res.write('Refresh Interval (minutes): <input type="text" name="refresh_interval" value="30"><br><br>');
-      res.write('<button>Save Configuration</button>');
-      res.write('</form>');
-      res.write('</body>');
-      res.end('</html>');
+      res.end(`Refresh Interval (minutes): <input type="text" name="refresh_interval" value="30"><br><br>
+               <button>Save Configuration</button>
+               </form>
+               </body>
+               </html>`);
     });
 });
 
